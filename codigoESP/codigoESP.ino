ca6251274ce5e3762d8d8f3a1ac87d9147a661d5
alvarcamachom <a.camacho@edu.uah.es>
2024-11-05 20:11:09 +0100

    DHT11

--- codigoESP/codigoESP.ino
+++ codigoESP/codigoESP.ino
@@ -5,7 +5,7 @@
 #define SSONIDO 35
 #define LIGHT_SENSOR 12
 #define BUTTON 21
-#define DHTPIN 34
+#define DHTPIN 3
 #define DHTTYPE DHT11 
 DHT dht(DHTPIN, DHTTYPE);
 #include <cstdio>
@@ -23,31 +23,12 @@ void setup() {
 }
 
 void alternating_leds(){
-  // put your main code here, to run repeatedly:
   digitalWrite(RED_LED, HIGH);  // Turn on the LED
   digitalWrite(GREEN_LED, LOW);
   delay(1000);                  // Wait for 1 second
   digitalWrite(RED_LED, LOW);   // Turn off the LED
   digitalWrite(GREEN_LED, HIGH);
   delay(1000);                  // Wait for 1 second
-}
-
-void change_led_when_button(){
-  if(digitalRead(BUTTON)){
-    digitalWrite(RED_LED, HIGH);
-    digitalWrite(GREEN_LED, LOW);
-  } else {
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, HIGH);
-  }
-}
-
-void buzz_when_button() {
-  if (digitalRead(BUTTON)) {
-    tone(BUZZER, 100); // Inicia el sonido a 100 Hz
-  } else {
-    noTone(BUZZER);    // Detiene el sonido cuando el botón no está presionado
-  }
 }
 
 void read_light_sensor() {
@@ -65,49 +46,25 @@ void read_sound_sensor() {
 }
 
 void readDHT(){
-      // Esperamos 5 segundos entre medidas
+  // Esperamos 5 segundos entre medidas
   delay(5000);
- 
-  // Leemos la humedad relativa
   float h = dht.readHumidity();
-  // Leemos la temperatura en grados centígrados (por defecto)
   float t = dht.readTemperature();
-  // Leemos la temperatura en grados Fahrenheit
-  float f = dht.readTemperature(true);
- 
-  // Comprobamos si ha habido algún error en la lectura
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Error obteniendo los datos del sensor DHT11");
-    /*Serial.println("Sensor h",isnan(h));
-    Serial.println("Sensor h",isnan(t));
-    Serial.println("Sensor h",isnan(f));*/
-    return;
+
+  if (isnan(h) || isnan(t)) {
+  Serial.println("Failed to read from DHT sensor!");
+  return;
   }
- 
-  // Calcular el índice de calor en Fahrenheit
-  float hif = dht.computeHeatIndex(f, h);
-  // Calcular el índice de calor en grados centígrados
-  float hic = dht.computeHeatIndex(t, h, false);
- 
   Serial.print("Humedad: ");
-  Serial.print(h);
-  Serial.print(" %\t");
+  Serial.println(h);
   Serial.print("Temperatura: ");
   Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Índice de calor: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
+  Serial.println("°C ");
 }
 
 void loop() {
     read_sound_sensor();            
     read_light_sensor();   
     readDHT();
-    digitalWrite(GREEN_LED, HIGH); 
-    buzz_when_button();
+    
 }
