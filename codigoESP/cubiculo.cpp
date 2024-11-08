953346ec2850525561ad8f3db583b9daad1234c2
krisMG21 <cristimg9@gmail.com>
2024-11-08 13:52:58 +0100

    Cosas movidas de sala a cubiculo
    Que estaba poniendo los componentes donde no era

--- codigoESP/cubiculo.cpp
+++ codigoESP/cubiculo.cpp
@@ -1,5 +1,22 @@
 #include "cubiculo.h"
 
-Cubiculo::Cubiculo() {
-  
+/** Recorre todos los componentes del cubículo.
+ *  Una vez una condición se cumple, ejecuta su código
+ *  y sale de la función, por optimización.
+*/
+void Cubiculo::update() {
+    // Boton presionado, empieza pomodoro
+    if (button.isPressed()) {
+        if(timer.isTimerRunning()) {
+            timer.reset();
+        }
+        else {
+            timer.start();
+        }
+    }
+    // Sensor de posicion, cubiculo ocupado
+    else if(s_posicion.ocupado()){
+        leds.set_ocupado(true);
+    }
+    leds.update();
 }
--- codigoESP/cubiculo.h
+++ codigoESP/cubiculo.h
@@ -1,4 +1,42 @@
+#ifndef SALA_H
+#define SALA_H
+
+#include "leds.h"
+#include "timer.h"
+#include "sensor.h"
+#include "button.h"
+
 class Cubiculo {
-  public:
-    Cubiculo();
+private:
+    bool occupied;
+    Leds leds;
+    Sensor s_luz;
+    Sensor s_sonido;
+    Sensor_US s_posicion;
+    Sensor_DHT s_dht;
+    Button button;
+    // Ventilador
+    Timer timer;
+
+public:
+    Cubiculo(
+        const Leds& leds,
+        const Sensor& s_luz,
+        const Sensor& s_sonido,
+        const Sensor_US& s_posicion,
+        const Sensor_DHT& s_dht,
+        const Button& button):
+        leds(leds),
+        s_luz(s_luz),
+        s_sonido(s_sonido),
+        s_posicion(s_posicion),
+        s_dht(s_dht),
+        button(button) {
+        occupied = false;
+    };
+
+    void update();
+
+
 };
+#endif //SALA_H
